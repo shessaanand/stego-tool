e74f1f3fded49fe7a9409a1e65a3c0f04fba063c
shessaanand <shessaasiva@gmail.com>
2026-05-20 16:52:55 +0530

    Add CLI-based steganography utility

--- main.cpp
+++ main.cpp
@@ -1,23 +1,101 @@
 #include "core/include/image.hpp"
 #include "core/include/steganography.hpp"
+
 #include <iostream>
+#include <string>
 
-int main(){
-    Image img("../sample.png");
-    std::cout << "Width: " << img.width << "\n";
-    std::cout << "Height: " << img.height << "\n";
-    std::cout << "Channels: " << img.channels << "\n";
-    std::string secretMessage = "Hello World";
-    Steganography::encode(img, secretMessage);
+int main(int argc, char* argv[]){
+    if (argc < 2){
+        std::cout
+            << "Usage:\n"
+            << "./stego encode <input> <output> <message>\n"
+            << "./stego decode <image>\n";
 
-    if (!img.save("encoded.png")){
-        std::cout << "Failed to save image\n";
         return 1;
     }
 
-    std::cout << "Saved encoded.png\n";
-    Image encodedImage("encoded.png");
-    std::string decoded = Steganography::decode(encodedImage);
-    std::cout << "Decoded message: "<< decoded << "\n";
+    std::string mode = argv[1];
+
+    if (mode == "encode"){
+        if (argc < 5){
+            std::cout
+                << "Usage:\n"
+                << "./stego encode <input> <output> <message>\n";
+
+            return 1;
+        }
+
+        std::string inputImage = argv[2];
+        std::string outputImage = argv[3];
+        std::string message = argv[4];
+
+        Image img(inputImage);
+
+        if (img.data.empty()){
+            std::cout << "Failed to load image\n";
+            return 1;
+        }
+
+        size_t maxBytes =
+            (img.data.size() - 32) / 8;
+
+        std::cout
+            << "Maximum capacity: "
+            << maxBytes
+            << " bytes\n";
+
+        if (message.size() > maxBytes){
+            std::cout
+                << "Message exceeds image capacity\n";
+
+            return 1;
+        }
+
+        Steganography::encode(img, message);
+
+        if (!img.save(outputImage)){
+            std::cout << "Failed to save image\n";
+            return 1;
+        }
+
+        std::cout
+            << "Encoded image saved to "
+            << outputImage
+            << "\n";
+    }
+
+    else if (mode == "decode"){
+        if (argc < 3){
+            std::cout
+                << "Usage:\n"
+                << "./stego decode <image>\n";
+
+            return 1;
+        }
+
+        std::string imagePath = argv[2];
+
+        Image img(imagePath);
+
+        if (img.data.empty()){
+            std::cout << "Failed to load image\n";
+            return 1;
+        }
+
+        std::string decoded =
+            Steganography::decode(img);
+
+        std::cout
+            << "Decoded message: "
+            << decoded
+            << "\n";
+    }
+
+    else{
+        std::cout
+            << "Invalid mode\n"
+            << "Use encode or decode\n";
+    }
+
     return 0;
 }
